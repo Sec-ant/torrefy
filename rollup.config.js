3a8c57101e568f9e5caf6b3f75e9de1fecc65d28
Ze-Zheng Wu <zzwu@zju.edu.cn>
2022-08-03 18:09:37 +0800

    remove commonjs build

--- rollup.config.js
+++ rollup.config.js
@@ -32,17 +32,11 @@ export default [
     plugins,
   },
 
-  // CommonJS (for Node) and ES module (for bundlers) build.
-  // (We could have three entries in the configuration array
-  // instead of two, but it's quicker to generate multiple
-  // builds from a single configuration where possible, using
-  // an array for the `output` option, where we can specify
-  // `file` and `format` for each target)
+  // ES module (for bundlers) build
   {
     input: "src/index.ts",
     external: ["workbox-streams"],
     output: [
-      { file: pkg.main, format: "cjs", sourcemap: true },
       { file: pkg.module, format: "es", sourcemap: true },
     ],
     plugins,
