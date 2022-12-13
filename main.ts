6ec6aa0c10be31a88691add85c0ab306377dedb0
Ze-Zheng Wu <zzwu@zju.edu.cn>
2022-12-13 11:34:00 +0800

    update: dev page

--- main.ts
+++ main.ts
@@ -1,16 +1,37 @@
 import { populateFileTree } from "./src/index.js";
 
-const testFile = new File(
-  ["Hello world. This is a test file"],
-  "test-file.txt",
-  {}
+const singleFileInput =
+  document.querySelector<HTMLInputElement>("#single-file-input");
+if (singleFileInput) {
+  singleFileInput.onchange = onFileInputChange;
+}
+
+const mutiFileInput =
+  document.querySelector<HTMLInputElement>("#multi-file-input");
+if (mutiFileInput) {
+  mutiFileInput.onchange = onFileInputChange;
+}
+
+const webkitDirectoryFileInput = document.querySelector<HTMLInputElement>(
+  "#webkitdirectory-file-input"
 );
+if (webkitDirectoryFileInput) {
+  webkitDirectoryFileInput.onchange = onFileInputChange;
+}
 
-const { fileTree, traverseTree, totalFileSize, totalFileCount } =
-  await populateFileTree([testFile, testFile]);
-
-const nodes = traverseTree(fileTree);
-
-for (const node of nodes) {
-  console.log(node);
+async function onFileInputChange(event: Event) {
+  const inputElement = event.currentTarget as HTMLInputElement;
+  const { files } = inputElement;
+  if (!files || files.length === 0) {
+    return;
+  }
+  const { fileTree, traverseTree, totalFileSize, totalFileCount } =
+    await populateFileTree(files);
+  console.log(inputElement.id);
+  console.log({
+    fileTree,
+    traversal: [...traverseTree(fileTree)],
+    totalFileSize,
+    totalFileCount,
+  });
 }
